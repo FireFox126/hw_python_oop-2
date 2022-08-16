91a902afb49ee0694563511c89d15db195895114
Sergey Ledenev <firefox_126@mail.ru>
2022-08-16 21:42:06 +0300

    Проверил код на наличие лишних пустых строк

--- homework.py
+++ homework.py
@@ -1,6 +1,6 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    # инициализация класса, создание нужных переменных
+
     def __init__(self, training_type: str,
                  duration: float,
                  distance: float,
@@ -12,8 +12,7 @@ class InfoMessage:
         self.speed = speed
         self.calories = calories
         self.duration = duration
-    
-    # возвращает данные переменных
+
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type}; '
                 f'Длительность: {self.duration:.3f} ч.; '
@@ -21,9 +20,8 @@ class InfoMessage:
                 f'Ср. скорость: {self.speed:.3f} км/ч; '
                 f'Потрачено ккал: {self.calories:.3f}.')
 
-# класс содержащий информацию о тренировке
+
 class Training:
-    # константы
     """Базовый класс тренировки."""
     LEN_STEP = 0.65
     M_IN_KM = 1000
@@ -38,7 +36,6 @@ class Training:
         self.duration = duration
         self.weight = weight
 
-    # геттеры, возращают определенную переменную
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
         distance = self.action * self.LEN_STEP / self.M_IN_KM
@@ -52,8 +49,7 @@ class Training:
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         pass
-    
-    # Геттер, возращающий InfoMessage
+
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
         info_message = InfoMessage(self.__class__.__name__,
@@ -63,14 +59,12 @@ class Training:
                                    self.get_spent_calories())
         return info_message
 
-class Running(Training): # Класс бега который наследует класс тренировки
-    # добавляются новые константы, старые из Training наследуются
+class Running(Training):
     """Тренировка: бег."""
     CF_RUN_1 = 18
     CF_RUN_2 = 20
     TRAINING_TYPE = 'RUN'
 
-    # У него переписывается только этот геттер, остальные геттеры аналогичны из класса Training
     def get_spent_calories(self) -> float:
         cal = self.CF_RUN_1 * self.get_mean_speed() - self.CF_RUN_2
         calories = cal * self.weight / self.M_IN_KM * self.duration * 60
@@ -83,7 +77,7 @@ class SportsWalking(Training):
     CF_WALK_2 = 2
     CF_WALK_3 = 0.029
     TRAINING_TYPE = 'WLK'
-    # переписывается инициализация
+
     def __init__(self,
                  action: int,
                  duration: float,
@@ -128,21 +122,19 @@ class Swimming(Training):
         calories = calories_1 * self.CF_SW_2 * self.weight
         return calories
 
-# глобальная функция, возвращает класс с данными (звездочка *data при инициализации классов говорит о том что передаются данные в виде кортежа(как будто в круглых скобках))
-# т.е. не Swimming([720, 1, 80, 25, 40]), а Swimming(720, 1, 80, 25, 40)
+
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
     type_dict = {'SWM': Swimming, 'RUN': Running, 'WLK': SportsWalking}
     return type_dict[workout_type](*data)
 
-# Выводит на экран текущие поля тренировки, во всех наследниках есть функция show_training_info
+
 def main(training: Training) -> None:
     """Главная функция."""
     info = training.show_training_info()
     print(info.get_message())
 
-# условие которые выполнится при запуске программы, если этот код небыл импортирован, т.е. выполнится если была введена команда python Novy_textovy_dokument.py
-# если будет import Novy_textovy_dokument, то можно пользоваться классами тренировок но это условие не выполнитсся
+
 if __name__ == '__main__':
     packages = [
         ('SWM', [720, 1, 80, 25, 40]),
@@ -150,8 +142,6 @@ if __name__ == '__main__':
         ('WLK', [9000, 1, 75, 180]),
     ]
 
-    # создает классы тренировок и выводит инфу в консоль
-    # packages - массив из кортежей, workout_type - первый элемент кортежа - строка, data - второй элемент массив 
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
